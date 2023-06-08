c5d10a8575dcb77ca371c1d01a52d998e38b331b
slickwild1401 <116937194+slickwild1401@users.noreply.github.com>
2023-06-08 19:26:49 +0800

    Add files via upload

--- C81main.js
+++ C81main.js
@@ -1,70 +1,39 @@
 canvas=document.getElementById("myCanvas");
 
-mouse_event="";
-color="white";
 
-var radius="50";
-var width="1";
 
 ctx=canvas.getContext("2d");
 ctx.beginPath();
-ctx.strokeStyle=color;
-ctx.lineWidth=2;
+ctx.strokeStyle="grey";
+ctx.lineWidth=1;
+ctx.rect(150,143,430,200);
+ctx.stroke();
+
+
+
+//arc
+ctx.beginPath();
+ctx.strokeStyle="blue";
+ctx.lineWidth=1;
 ctx.arc(200,200,40,0,2 * Math.PI);
 ctx.stroke();
-
-
-canvas.addEventListener("mousedown", my_mousedown);
-function my_mousedown(e)
-{
- mouse_event="mousedown"
- color=document.getElementById("color").value;
-radius=document.getElementById("radius").value;
-width=document.getElementById("width").value;
-mouse_x=e.clientX-canvas.offsetLeft;
-mouse_y=e.clientY-canvas.offsetTop;
-circle(mouse_x, mouse_y);
-
-
-}
-canvas.addEventListener("mousemove", my_mousemove);
-function my_mousemove(e) {
-   
-   
-    mouse_event="mouseleave";
-    
-}
-canvas.addEventListener("mouseup", my_mouseup);
-function my_mouseup(e){
-   
-    
-    mouse_event="mouseup";
-
-}
-if ( mouse_event="mousedown"){
- 
-   ctx.beginPath();
-ctx.strokeStyle=color;
-ctx.lineWidth=width;
-ctx.arc(200,200,40,0,2 * Math.PI);
+ctx.beginPath();
+ctx.strokeStyle="yellow";
+ctx.lineWidth=1;
+ctx.arc(240,250,40,0,2 * Math.PI);
 ctx.stroke();
-
-}
-
-
-
-function circle(mouse_x,mouse_y)
-{
-    ctx.beginPath();
-ctx.strokeStyle=color;
-ctx.lineWidth=2;
-ctx.arc(mouse_x,mouse_y,40,0,2 * Math.PI);
+ctx.beginPath();
+ctx.strokeStyle="black";
+ctx.lineWidth=1;
+ctx.arc(286,200,40,0,2 * Math.PI);
 ctx.stroke();
-
-}
-
-
-
-
-
-
+ctx.beginPath();
+ctx.strokeStyle="green";
+ctx.lineWidth=1;
+ctx.arc(326,250,40,0,2 * Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.strokeStyle="red";
+ctx.lineWidth=1;
+ctx.arc(376,200,40,0,2 * Math.PI);
+ctx.stroke();
